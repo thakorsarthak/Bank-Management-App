49a99921fb2cfe64cc8e20cc4f532cee420f7196
thakorsarthak <thakorsarthak618@gmail.com>
2026-04-08 11:19:38 +0530

    Update BankManagementAppApplicationTests.java

--- Backend/BankManagementApp/src/test/java/com/example/bankapp/BankManagementAppApplicationTests.java
+++ Backend/BankManagementApp/src/test/java/com/example/bankapp/BankManagementAppApplicationTests.java
@@ -3,11 +3,9 @@ package com.example.bankapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BankManagementAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
