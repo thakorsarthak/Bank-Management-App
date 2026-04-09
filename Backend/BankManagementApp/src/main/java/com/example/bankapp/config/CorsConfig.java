b5a974a1688ff5ed3c8a14a3c798e36293cd2e93
thakorsarthak <thakorsarthak618@gmail.com>
2026-04-09 15:32:59 +0530

    Update CorsConfig.java

--- Backend/BankManagementApp/src/main/java/com/example/bankapp/config/CorsConfig.java
+++ Backend/BankManagementApp/src/main/java/com/example/bankapp/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
                         .allowedOrigins(
                         "http://localhost:60001", // Angular dev server
                         "http://localhost:4200",
-                        "${FRONTEND_URL:https://bank-management-eight.vercel.app}" 
+                        "${FRONTEND_URL:https://bank-management-thakorsarthak2912-8751s-projects.vercel.app/}" 
                         )
                         .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type", "Accept")
