d0943873048bd9d72bc6a3f54ba33ed057336c9c
thakorsarthak <thakorsarthak618@gmail.com>
2026-04-09 18:54:35 +0530

    added redis test directly inside springapp

--- Backend/BankManagementApp/src/main/java/com/example/bankapp/BankManagementAppApplication.java
+++ Backend/BankManagementApp/src/main/java/com/example/bankapp/BankManagementAppApplication.java
@@ -1,17 +1,35 @@
 package com.example.bankapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
 public class BankManagementAppApplication {
-
+	
+	 @Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BankManagementAppApplication.class, args);
 	}
+	
+
+    @PostConstruct
+    public void testRedis() {
+        try {
+            redisTemplate.opsForValue().set("test", "working");
+            System.out.println("✅ Redis connected successfully");
+        } catch (Exception e) {
+            System.out.println("❌ Redis connection failed: " + e.getMessage());
+        }
+    }
 }
