abd6741332de7227d9a15d43b19f6a0c7145373c
thakorsarthak <thakorsarthak618@gmail.com>
2026-04-09 15:27:20 +0530

    updated environment

--- frontend/src/app/core-component/services/account.service.ts
+++ frontend/src/app/core-component/services/account.service.ts
@@ -14,21 +14,21 @@ private apiUrl = environment.apiUrl;
 constructor(private http: HttpClient) { }
 
   createAccount(obj:any):Observable<any[]>{
-      return this.http.post<any>(`${this.apiUrl}/bankapp/main/create`,obj)
+      return this.http.post<any>(`${this.apiUrl}/main/create`,obj)
   }
   loginAccount(obj:any):Observable<any>{
-      return this.http.post<any>(`${this.apiUrl}/bankapp/main/login-account`,obj)
+      return this.http.post<any>(`${this.apiUrl}/main/login-account`,obj)
   }
 
   sendOtp(obj:any):Observable<any[]>{
-      return this.http.post<any>(`${this.apiUrl}/bankapp/otp/send`,obj)
+      return this.http.post<any>(`${this.apiUrl}/otp/send`,obj)
   }
 
   verifyOtp(obj:any):Observable<any[]>{
-      return this.http.post<any>(`${this.apiUrl}/bankapp/otp/verify`,obj)
+      return this.http.post<any>(`${this.apiUrl}/otp/verify`,obj)
   }
    setPinWithOtp(obj:any):Observable<any[]>{
-      return this.http.put<any>(`${this.apiUrl}/bankapp/account/changePasswordWithOtp`,obj)
+      return this.http.put<any>(`${this.apiUrl}/account/changePasswordWithOtp`,obj)
   }
  
 }
